9f27867e555769666f5c48f19fdb2ef640a07afd
alexmwirigi <mwirigialex351@gmail.com>
2025-10-19 16:59:10 +0300

    update

--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import connectDB from "./db";
-import User from "../models/user";
+import User from "../models/user.js";
+
 
 
 // Create a client to send and receive events
